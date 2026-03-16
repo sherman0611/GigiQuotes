b17d9e53cd824b0c92bab2f3e2a3113bf042729c
Gigi Quotes <sherman0611@gmail.com>
2026-03-16 01:13:39 +0000

    connection fix

--- backend/app.py
+++ backend/app.py
@@ -20,12 +20,7 @@ SIXSEVEN_WORDS = ["6 7", "Six Seven"]
 app = Flask(__name__)
 
 def get_db_connection():
-    return psycopg2.connect(
-        host = os.getenv('DB_HOST'),
-        database = os.getenv('DB_NAME'),
-        user = os.getenv('DB_USER'),
-        password = os.getenv('DB_PASSWORD')
-    )
+    return psycopg2.connect(os.getenv('DATABASE_URL'))
 
 def serialize_row(row):
     result = dict(row)
