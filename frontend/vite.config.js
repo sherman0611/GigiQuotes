81e8b2e44663242a1b5e1acfeb990d21f4d19a29
Gigi Quotes <sherman0611@gmail.com>
2026-03-17 22:18:01 +0000

    hmm

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,25 +3,25 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    allowedHosts: ['gigiquotes.com', 'www.gigiquotes.com'],
-    hmr: {
-      host: 'gigiquotes.com',
-      protocol: 'wss',
-      clientPort: 443
-    },
-    watch: {
-      usePolling: true,
-    },
-    proxy: {
-      '/api': {
-        target: 'http://backend:5000',
-        // target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-      }
-    }
-  }
+  // server: {
+  //   host: true,
+  //   port: 5173,
+  //   strictPort: true,
+  //   allowedHosts: ['gigiquotes.com', 'www.gigiquotes.com'],
+  //   hmr: {
+  //     host: 'gigiquotes.com',
+  //     protocol: 'wss',
+  //     clientPort: 443
+  //   },
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://backend:5000',
+  //       // target: 'http://127.0.0.1:5000',
+  //       changeOrigin: true,
+  //     }
+  //   }
+  // }
 })
